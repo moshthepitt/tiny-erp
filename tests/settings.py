e7d4ab8b848f917bb2c6f42fae346e63c1ecbf02
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-16 21:17:58 +0300

    Add middleware

--- tests/settings.py
+++ tests/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'small_small_hr',
+        'NAME': 'tiny_erp',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1'
@@ -48,6 +48,16 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 TIME_ZONE = 'Africa/Nairobi'
