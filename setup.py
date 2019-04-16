67d11e3f7db97bd7375fcfdc38300520b934bca0
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-04-16 21:11:21 +0300

    Add long description

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 """
 Setup.py for django-tiny-erp
 """
-from setuptools import setup, find_packages
+from os import path
+
+from setuptools import find_packages, setup
+
+# read the contents of your README file
+with open(
+        path.join(path.abspath(path.dirname(__file__)), "README.md"),
+        encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
 
 SMALL_SMALL_HR_VERSION = __import__('tiny_erp').__small_small_hr_version__
 
@@ -9,6 +17,8 @@ setup(
     name='django-tiny-erp',
     version=__import__('tiny_erp').__version__,
     description='Enterprise Resource Planning (ERP) for tiny companies',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     license='MIT',
     author='Kelvin Jayanoris',
     author_email='kelvin@jayanoris.com',
@@ -24,4 +34,3 @@ setup(
         'Programming Language :: Python :: 3.7'
     ],
 )
-
