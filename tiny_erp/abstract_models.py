d283b5d8b54526f1d966e0b8a5859df1dc449f17
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-05-15 22:14:04 +0300

    Add AbstractLineItem

--- tiny_erp/abstract_models.py
+++ tiny_erp/abstract_models.py
@@ -21,6 +21,39 @@ class AbstractEntity(models.Model):
         return self.name
 
 
+class AbstractLineItem(models.Model):
+    """Model definition for abstract line item"""
+
+    name = models.CharField(_("Name"), max_length=255)
+    description = models.TextField(_('Description'), blank=True, default='')
+    quantity = models.PositiveIntegerField(_("Quantity"), default=1)
+    price = models.DecimalField(
+        _("Price"), help_text=_("The price per item"), max_digits=64,
+        decimal_places=2)
+
+    def _get_total(self):
+        """Get the total for this line item
+
+        Returns:
+            Decimal -- the total for this line item
+        """
+        return self.price * self.quantity
+
+    @property
+    def total(self):
+        """the total for this line item
+
+        Returns:
+            Decimal -- the total for this line item
+        """
+        return self._get_total()
+
+    class Meta:
+        """Meta definition for AbstractLineItem."""
+
+        abstract = True
+
+
 class TimeStampedAbstractEntity(TimeStampedModel, AbstractEntity):
     """Model definition for Business."""
 
