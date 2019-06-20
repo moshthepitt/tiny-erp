b0cc465efc53da220e9b7c6952641caea06abf24
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-20 20:52:44 +0300

    Add RequisitionLineItemForm

--- tiny_erp/apps/purchases/forms.py
+++ tiny_erp/apps/purchases/forms.py
@@ -3,7 +3,17 @@ Forms module for tiny erp
 """
 from django import forms
 
-from tiny_erp.apps.purchases.models import Requisition
+from tiny_erp.apps.purchases.models import Requisition, RequisitionLineItem
+
+
+class RequisitionLineItemForm(forms.ModelForm):
+    """Form definition for RequisitionLineItem."""
+
+    class Meta:
+        """Meta definition for RequisitionLineItemform."""
+
+        model = RequisitionLineItem
+        fields = ["item", "quantity", "price"]
 
 
 class RequisitionForm(forms.ModelForm):
