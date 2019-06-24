c431e82d7d2b7205986b5e4ab23deaf5ac917ac7
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-24 21:46:17 +0300

    Cleanup

--- tiny_erp/layout.py
+++ tiny_erp/layout.py
@@ -1,7 +1,7 @@
 """Custom crispy forms layout objects"""
 from django.template.loader import render_to_string
 
-from crispy_forms.layout import TEMPLATE_PACK, LayoutObject
+from crispy_forms.layout import LayoutObject
 
 
 class Formset(LayoutObject):
@@ -10,14 +10,21 @@ class Formset(LayoutObject):
     template = "tiny_erp/formset.html"
 
     def __init__(self, formset_in_context: str, template: str = None):
-        """init method"""
+        """[summary]
+
+        Arguments:
+            formset_in_context {str} -- The Formset object/class
+
+        Keyword Arguments:
+            template {str} -- The formset custom template path (default: {None})
+        """
         self.formset_in_context = formset_in_context
         if template:
             self.template = template
 
     def render(  # pylint: disable=unused-argument,bad-continuation
-        self, form, form_style, context, template_pack=TEMPLATE_PACK
+        self, *args, **kwargs
     ):
-        """render method"""
+        """the render method"""
 
         return render_to_string(self.template, {"formset": self.formset_in_context})
