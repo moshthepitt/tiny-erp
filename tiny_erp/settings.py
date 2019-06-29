9e2629faa43e8a3c352e8351b6150d00f2f940c6
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-29 13:07:33 +0300

    Add email settings

--- tiny_erp/settings.py
+++ tiny_erp/settings.py
@@ -1,4 +1,11 @@
-"""settings module"""
+"""
+Configurable options
+"""
+from django.conf import settings
+
 # strings
 TINY_ERP_REQUISITION_ITEMS_TXT = "Requisition Items"
 TINY_ERP_SUBMIT_TXT = "Submit"
+# emails
+TINY_ERP_ADMIN_NAME = "ERP"
+TINY_ERP_ADMIN_EMAILS = [settings.DEFAULT_FROM_EMAIL]
