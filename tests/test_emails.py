2ddf9fdc00383be79e61b5e56ba29cad57e0b9ae
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-29 17:15:41 +0300

    Add test for requisition_updated_email

--- tests/test_emails.py
+++ tests/test_emails.py
@@ -8,11 +8,16 @@ from django.test import TestCase, override_settings
 
 from model_mommy import mommy
 
-from tiny_erp.apps.purchases.emails import requisition_filed_email
+from tiny_erp.apps.purchases.emails import (
+    requisition_filed_email,
+    requisition_updated_email,
+)
 
 
 @override_settings(
-    TINY_ERP_ADMIN_EMAILS=["erp@example.com"], TINY_ERP_ADMIN_NAME="mosh"
+    TINY_ERP_ADMIN_EMAILS=["erp@example.com"],
+    TINY_ERP_ACCOUNTS_EMAILS=["accounts@example.com"],
+    TINY_ERP_ADMIN_NAME="mosh",
 )
 class TestEmails(TestCase):
     """
@@ -73,6 +78,52 @@ class TestEmails(TestCase):
         self.assertEqual(
             "Hello,<br/><br/><p>There has been a new purchase "
             "requisition.  Please log in to process it.</p><br/><br/>"
+            "Thank you,<br/>example.com<br/>------<br/>http://example.com",
+            mail.outbox[0].alternatives[0][0],
+        )
+
+    def test_requisition_updated_email(self):
+        """
+        Test requisition_updated_email
+        """
+        requisition = mommy.make(
+            "purchases.Requisition",
+            staff=self.staffprofile,
+            location=self.location,
+            business=self.business,
+            department=self.department,
+            date_placed="2019-03-03",
+            date_required="2019-03-10",
+            reason="Science, bitch",
+        )
+
+        with patch("tiny_erp.apps.purchases.emails.send_email") as mock:
+            requisition_updated_email(requisition)
+            mock.assert_called_with(
+                name="Bob Ndoe",
+                email="bob@example.com",
+                subject=f"Purchase Requisition Updated - #{requisition.id}",
+                message="Your purchase requisition has been updated.  Please log in to view it.",  # noqa  pylint: disable=line-too-long
+                obj=requisition,
+                template="generic",
+                template_path="tiny_erp/email",
+            )
+
+        requisition_updated_email(requisition)
+        self.assertEqual(1, len(mail.outbox))
+        self.assertEqual(
+            f"Purchase Requisition Updated - #{requisition.id}", mail.outbox[0].subject
+        )
+        self.assertEqual(["Bob Ndoe <bob@example.com>"], mail.outbox[0].to)
+        self.assertEqual(
+            "Hello,\n\nYour purchase requisition has been updated.  Please "
+            "log in to view it.\n\nThank you,\n\n"
+            "example.com\n------\nhttp://example.com\n",
+            mail.outbox[0].body,
+        )
+        self.assertEqual(
+            "Hello,<br/><br/><p>Your purchase requisition has been updated."
+            "  Please log in to view it.</p><br/><br/>"
             "Thank you,<br/>example.com<br/>------<br/>http://example.com",
             mail.outbox[0].alternatives[0][0],
         )
