3348f4ae7a02ac4fa38c94d5db04f916d802da13
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-06-29 16:46:31 +0300

    Add accounts email to settings

--- tiny_erp/settings.py
+++ tiny_erp/settings.py
@@ -9,5 +9,6 @@ TINY_ERP_SUBMIT_TXT = "Submit"
 # emails
 TINY_ERP_ADMIN_NAME = "ERP"
 TINY_ERP_ADMIN_EMAILS = [settings.DEFAULT_FROM_EMAIL]
+TINY_ERP_ACCOUNTS_EMAILS = [settings.DEFAULT_FROM_EMAIL]
 # TINY_ERP_REQUISITION_FILED_EMAIL_TXT - body of requisition filed email
 # TINY_ERP_REQUISITION_FILED_EMAIL_SUBJ - subject of requisition filed email
