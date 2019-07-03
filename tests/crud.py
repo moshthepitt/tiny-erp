fa806764126027fc5e6419737398be0a5c0aad93
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-07-03 23:57:02 +0300

    Use UpdateRequisitionForm for updates

--- tests/crud.py
+++ tests/crud.py
@@ -1,7 +1,7 @@
 """CRUD module"""
 from vega_admin.views import VegaCRUDView
 
-from tiny_erp.apps.purchases.forms import RequisitionForm
+from tiny_erp.apps.purchases.forms import RequisitionForm, UpdateRequisitionForm
 from tiny_erp.apps.purchases.models import Requisition
 
 
@@ -14,6 +14,8 @@ class RequisitionCRUD(VegaCRUDView):
     protected_actions = None
     permissions_actions = None
     form_class = RequisitionForm
+    create_form_class = RequisitionForm
+    update_form_class = UpdateRequisitionForm
 
 
 urlpatterns = RequisitionCRUD().url_patterns()
