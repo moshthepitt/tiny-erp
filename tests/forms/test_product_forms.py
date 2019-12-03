c6adabe27a254ff1120d254b834d2817f8f82a24
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-12-03 20:22:27 +0300

    Test HTML output of Supplier Form

--- tests/forms/test_product_forms.py
+++ tests/forms/test_product_forms.py
@@ -3,9 +3,51 @@ from django.test import RequestFactory, TestCase
 
 from tiny_erp.apps.products.forms import SupplierForm
 
+CREATE_SUPPLIER_FORM = """
+<p>
+    <label for="id_name">Name:</label>
+    <input type="text" name="name" maxlength="2000" required id="id_name">
+</p>
+<p>
+    <label for="id_contact_person">Contact Person:</label>
+    <input type="text" name="contact_person" maxlength="2000" required id="id_contact_person">
+</p>
+<p>
+    <label for="id_emails">Email Address(es):</label>
+    <textarea name="emails" cols="40" rows="2" id="id_emails"></textarea>
+    <span class="helptext">Enter a comma-separated list of email addresses</span></p>
+<p>
+    <label for="id_phones">Phone Number(s):</label>
+    <textarea name="phones" cols="40" rows="2" id="id_phones"></textarea>
+    <span class="helptext">Enter a comma-separated list of phone numbers</span>
+</p>
+"""  # noqa
+
+UPDATE_SUPPLIER_FORM = """
+<p>
+    <label for="id_name">Name:</label>
+    <input type="text" name="name" value="Umbrella Inc" maxlength="2000" required id="id_name">
+</p>
+<p>
+    <label for="id_contact_person">Contact Person:</label>
+    <input type="text" name="contact_person" value="Alice" maxlength="2000" required id="id_contact_person">
+</p>
+<p>
+    <label for="id_emails">Email Address(es):</label>
+    <textarea name="emails" cols="40" rows="2" id="id_emails">b@example.com</textarea>
+    <span class="helptext">Enter a comma-separated list of email addresses</span></p>
+<p>
+    <label for="id_phones">Phone Number(s):</label>
+    <textarea name="phones" cols="40" rows="2" id="id_phones">+254711000000,+254722000000</textarea>
+    <span class="helptext">Enter a comma-separated list of phone numbers</span>
+</p>
+"""  # noqa
+
 
 class TestProductForms(TestCase):
     """Test class for product forms."""
+
+    maxDiff = None
 
     def setUp(self):
         """Setup test class."""
@@ -13,6 +55,7 @@ class TestProductForms(TestCase):
 
     def test_supplier_form(self):
         """Test SupplierForm."""
+        self.assertHTMLEqual(CREATE_SUPPLIER_FORM, SupplierForm().as_p())
         data = {
             "name": "Umbrella Inc",
             "contact_person": "Marvin",
@@ -35,6 +78,7 @@ class TestProductForms(TestCase):
         }
 
         form = SupplierForm(instance=supplier, data=update_data)
+        self.assertHTMLEqual(UPDATE_SUPPLIER_FORM, form.as_p())
         self.assertTrue(form.is_valid())
         form.save()
         supplier.refresh_from_db()
