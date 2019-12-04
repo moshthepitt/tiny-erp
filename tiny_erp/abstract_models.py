40703aedee6f12ca3bb1f5a78cc330ebadd895c4
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-12-04 22:37:09 +0300

    Use MoneyField for price

--- tiny_erp/abstract_models.py
+++ tiny_erp/abstract_models.py
@@ -1,8 +1,11 @@
 """module for abstract models"""
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext as _
 
-from small_small_hr.models import TimeStampedModel
+from vega_admin.mixins import TimeStampedModel
+
+from django_prices.models import MoneyField
 
 
 class AbstractEntity(models.Model):
@@ -25,11 +28,27 @@ class AbstractLineItem(models.Model):
     """Model definition for abstract line item"""
 
     name = models.CharField(_("Name"), max_length=255)
-    description = models.TextField(_('Description'), blank=True, default='')
-    quantity = models.PositiveIntegerField(_("Quantity"), default=1)
-    price = models.DecimalField(
-        _("Price"), help_text=_("The price per item"), max_digits=64,
-        decimal_places=2)
+    description = models.TextField(_("Description"), blank=True, default="")
+    quantity = models.DecimalField(
+        _("Quantity"), max_digits=64, decimal_places=2, default=1
+    )
+    currency = models.CharField(
+        _("Currency"),
+        max_length=3,
+        choices=getattr(
+            settings, "TINY_ERP_AVAILABLE_CURRENCIES", [("KES", "Kenya Shilling")]
+        ),
+        default=getattr(settings, "TINY_ERP_DEFAULT_CURRENCY", "KES"),
+        db_index=True,
+    )
+    internal_price = models.DecimalField(
+        _("Price"), help_text=_("The price per item"), max_digits=64, decimal_places=2
+    )
+    price = MoneyField(
+        verbose_name=_("Price"),
+        amount_field="internal_price",
+        currency_field="currency",
+    )
 
     def _get_total(self):
         """Get the total for this line item
@@ -37,7 +56,7 @@ class AbstractLineItem(models.Model):
         Returns:
             Decimal -- the total for this line item
         """
-        return self.price * self.quantity
+        return self.internal_price * self.quantity
 
     @property
     def total(self):
