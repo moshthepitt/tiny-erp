a34addcb80d799b09c05b02586507f6ea48cb72f
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-12-08 10:41:04 +0300

    Cleanup

--- tiny_erp/__init__.py
+++ tiny_erp/__init__.py
@@ -1,7 +1,7 @@
 """
 Main init file for tiny_erp
 """
-__vega_admin__ = "0.0.16"
+__vega_admin_version__ = "0.0.16"
 __small_small_hr_version__ = "0.1.9"
 VERSION = (0, 0, 4)
 __version__ = ".".join(str(v) for v in VERSION)
