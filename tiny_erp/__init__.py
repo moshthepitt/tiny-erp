49e6ca799299c042a4fc2f8208585c9c6e7bea25
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-06-26 12:57:39 +0300

    Update packages

--- tiny_erp/__init__.py
+++ tiny_erp/__init__.py
@@ -4,5 +4,5 @@ Main init file for tiny_erp
 VERSION = (0, 1, 3)
 
 __vega_admin_version__ = "0.0.16"
-__small_small_hr_version__ = "0.1.9"
+__small_small_hr_version__ = "0.2.0"
 __version__ = ".".join(str(_) for _ in VERSION)
