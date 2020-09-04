52decb51e3e48a1c69ea1054f36e6a74ac438a05
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-04 21:31:25 +0300

    Change Requisition to work with model_reviews

--- tiny_erp/apps/purchases/models.py
+++ tiny_erp/apps/purchases/models.py
@@ -5,24 +5,16 @@ from django.utils.translation import ugettext as _
 
 from vega_admin.mixins import TimeStampedModel
 
+from model_reviews.models import AbstractReview
+
 from tiny_erp.abstract_models import TimeStampedAbstractLineItem
 from tiny_erp.apps.locations.models import Business, Department, Location
 from tiny_erp.apps.products.models import Product
 
 
 # pylint: disable=no-member
-class Requisition(TimeStampedModel):
+class Requisition(TimeStampedModel, AbstractReview):
     """Model definition for Requisition."""
-
-    APPROVED = "1"
-    REJECTED = "2"
-    PENDING = "3"
-
-    STATUS_CHOICES = (
-        (APPROVED, _("Approved")),
-        (PENDING, _("Pending")),
-        (REJECTED, _("Rejected")),
-    )
 
     staff = models.ForeignKey(
         "small_small_hr.StaffProfile",
@@ -39,16 +31,7 @@ class Requisition(TimeStampedModel):
     department = models.ForeignKey(
         Department, verbose_name=_("Department"), on_delete=models.PROTECT
     )
-    reason = models.TextField(_("Reason"), blank=False, default="")
-    status = models.CharField(
-        _("Status"),
-        max_length=1,
-        choices=STATUS_CHOICES,
-        default=PENDING,
-        blank=True,
-        db_index=True,
-    )
-    comments = models.TextField(_("Comments"), blank=True, default="")
+    review_reason = models.TextField(_("Reason"), blank=False, default="")
     date_placed = models.DateField(_("Date Placed"))
     date_required = models.DateField(_("Date Required"))
     total = models.DecimalField(
