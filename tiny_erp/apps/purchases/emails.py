fc56bd8c1b28c3db3ea472ea5d60c578cf41154c
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-07 22:01:48 +0300

    Add requisition review email functions

--- tiny_erp/apps/purchases/emails.py
+++ tiny_erp/apps/purchases/emails.py
@@ -2,9 +2,14 @@
 from django.conf import settings
 from django.utils.translation import ugettext as _
 
-from model_reviews.emails import send_email
+from model_reviews.emails import get_display_name, send_email
+from model_reviews.models import ModelReview, Reviewer
 
 from tiny_erp.apps.purchases.models import Requisition
+from tiny_erp.constants import (
+    REQUISITION_COMPLETED_EMAIL_TEMPLATE,
+    REQUISITION_FILED_EMAIL_TEMPLATE,
+)
 
 
 def requisition_filed_email(  # pylint: disable=bad-continuation
@@ -36,6 +41,22 @@ def requisition_filed_email(  # pylint: disable=bad-continuation
             obj=requisition_obj,
             template=template,
             template_path=template_path,
+        )
+
+
+def send_requisition_filed_email(reviewer: Reviewer):
+    """Send email when a purchase requisition is filed."""
+    if reviewer.user.email:
+        source = reviewer.review.content_object
+        send_email(
+            name=get_display_name(reviewer.user),
+            email=reviewer.user.email,
+            subject=source.review_request_email_subject,
+            message=source.review_request_email_body,
+            obj=reviewer.review,
+            cc_list=None,
+            template=source.email_template,
+            template_path=REQUISITION_FILED_EMAIL_TEMPLATE,
         )
 
 
@@ -99,3 +120,20 @@ def requisition_approved_email(  # pylint: disable=bad-continuation
             template=template,
             template_path=template_path,
         )
+
+
+def send_requisition_approved_email(review_obj: ModelReview):
+    """Send notice that purchase requisition is approved.."""
+    if not review_obj.needs_review() and review_obj.user:
+        if review_obj.user.email:
+            source = review_obj.content_object
+            send_email(
+                name=get_display_name(review_obj.user),
+                email=review_obj.user.email,
+                subject=source.review_complete_email_subject,
+                message=source.review_complete_email_body,
+                obj=review_obj,
+                cc_list=None,
+                template=source.email_template,
+                template_path=REQUISITION_COMPLETED_EMAIL_TEMPLATE,
+            )
