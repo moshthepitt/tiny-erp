e5bc1447f8bb106d41e913340b1a300a9ba95f8e
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-07 22:01:11 +0300

    Add template paths to constants.

--- tiny_erp/constants.py
+++ tiny_erp/constants.py
@@ -1,2 +1,4 @@
 """Contants module."""
 EMAIL_TEMPLATE_PATH = "tiny_erp/email"
+REQUISITION_FILED_EMAIL_TEMPLATE = "requisition_filed"
+REQUISITION_COMPLETED_EMAIL_TEMPLATE = "requisition_completed"
