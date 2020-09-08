147a4db6722583c0a64e52beba0f946fac8dd0ec
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-08 21:33:34 +0300

    Use levels when creating tiered reviewers

--- tiny_erp/apps/purchases/reviews.py
+++ tiny_erp/apps/purchases/reviews.py
@@ -6,14 +6,14 @@ from django.db import models
 from model_reviews.models import Reviewer
 
 
-def set_reviewer_by_email(email: str, review_obj: models.Model):
+def set_reviewer_by_email(email: str, review_obj: models.Model, level: int = 0):
     """Set reviewer using email address."""
     try:
         user = User.objects.get(email=email)
     except User.DoesNotExist:
         pass
     else:
-        reviewer = Reviewer(review=review_obj, user=user)
+        reviewer = Reviewer(review=review_obj, user=user, level=level)
         reviewer.save()  # ensure save method is called
 
 
@@ -26,5 +26,14 @@ def set_requisition_reviewer(review_obj: models.Model):
             # set the first person as the reviewer
             set_reviewer_by_email(reviewer_emails[0], review_obj)
         else:
-            for reviewer_email in reviewer_emails:
-                set_reviewer_by_email(reviewer_email, review_obj)
+            for idx, reviewer_email in enumerate(reviewer_emails):
+                set_reviewer_by_email(reviewer_email, review_obj, idx)
+
+
+# def set_next_reviewer(review_obj: models.Model):
+#     """Set the next reviewer."""
+#     reviewer_emails = settings.TINY_ERP_REQUISITION_REVIEWERS
+#     use_tiers = settings.TINY_ERP_REQUISITION_REVIEWS_TIERS
+#     if reviewer_emails and use_tiers:
+#         # first lets get all the reviewers who have already reviewed
+#         Reviewer.objects.filter(review=review_obj, )
