e78627ae56624b9277b04eac4707c9db5c0d85b3
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-08 21:22:10 +0300

    Add requisition reviewers settings

--- tiny_erp/settings.py
+++ tiny_erp/settings.py
@@ -1,4 +1,6 @@
 """Configurable options."""
+from typing import List
+
 from django.conf import settings
 
 TINY_ERP_DEFAULT_CURRENCY = "KES"
@@ -12,8 +14,13 @@ TINY_ERP_REQUISITION_FORMSET_ERROR_TXT = (
 )
 # emails
 TINY_ERP_ADMIN_NAME = "ERP"
-TINY_ERP_ADMIN_EMAILS = [settings.DEFAULT_FROM_EMAIL]
-TINY_ERP_ACCOUNTS_EMAILS = [settings.DEFAULT_FROM_EMAIL]
+# list of email addresses to review purchase requisitions
+TINY_ERP_REQUISITION_REVIEWERS: List[str] = []
+# If true we will have tiered levels of review i.e. TINY_ERP_REQUISITION_REVIEWERS[0]
+# must approve before TINY_ERP_REQUISITION_REVIEWERS[1] gets notified etc.
+TINY_ERP_REQUISITION_REVIEWS_TIERS: bool = False
+TINY_ERP_ADMIN_EMAILS = [settings.DEFAULT_FROM_EMAIL]  # should remove?
+TINY_ERP_ACCOUNTS_EMAILS = [settings.DEFAULT_FROM_EMAIL]  # should remove?
 # TINY_ERP_REQUISITION_FILED_EMAIL_TXT - body of requisition filed email
 # TINY_ERP_REQUISITION_FILED_EMAIL_SUBJ - subject of requisition filed email
 # TINY_ERP_REQUISITION_UPDATED_EMAIL_TXT - body of requisition updated email
