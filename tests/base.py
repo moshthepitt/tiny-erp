563a47942da036fa67ca6623476429255f106536
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-11 23:09:22 +0300

    Add test base for convenience

---
+++ tests/base.py
@@ -0,0 +1,19 @@
+"""Base test module."""
+from django.test import TestCase
+
+from model_bakery import baker
+
+
+class TestBase(TestCase):
+    """Base test class."""
+
+    def setUp(self):
+        """Set up."""
+        super().setUp()
+        self.user = baker.make(
+            "auth.User", first_name="Bob", last_name="Ndoe", email="bob@example.com"
+        )
+        self.staffprofile = baker.make("small_small_hr.StaffProfile", user=self.user)
+        self.business = baker.make("locations.Business", name="X Inc")
+        self.location = baker.make("locations.Location", name="Voi")
+        self.department = baker.make("locations.Department", name="Science")
