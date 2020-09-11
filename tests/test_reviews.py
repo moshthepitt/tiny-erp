3a8ce66e413eaf02b42407033a7975db61fd0921
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-12 01:04:34 +0300

    Test tiered reviewer process

--- tests/test_reviews.py
+++ tests/test_reviews.py
@@ -2,9 +2,10 @@
 from unittest.mock import patch
 
 from django.contrib.contenttypes.models import ContentType
-from django.test import override_settings
+from django.test import RequestFactory, override_settings
 
 from model_bakery import baker
+from model_reviews.forms import PerformReview
 from model_reviews.models import ModelReview, Reviewer
 
 from .base import TestBase
@@ -12,6 +13,11 @@ from .base import TestBase
 
 class TestReviews(TestBase):
     """Test reviews stuff."""
+
+    def setUp(self):
+        """Set up test class."""
+        super().setUp()
+        self.factory = RequestFactory()
 
     @override_settings(
         TINY_ERP_REQUISITION_REVIEWS_TIERS=True,
@@ -22,8 +28,8 @@ class TestReviews(TestBase):
         ],
     )
     @patch("tiny_erp.apps.purchases.reviews.send_requisition_filed_email")
-    def test_set_requisition_reviewer(self, mock):
-        """Test set_requisition_reviewer."""
+    def test_tiered_reviewers(self, mock):
+        """Test that tiered reviewers work as expected."""
         reviewer1 = baker.make(
             "auth.User", username="1@example.com", email="1@example.com"
         )
@@ -59,3 +65,72 @@ class TestReviews(TestBase):
         mock.assert_called_once_with(
             reviewer=Reviewer.objects.get(user=reviewer1, review=review, level=0,)
         )
+
+        # let reviewer1 approve it
+        request = self.factory.get("/")
+        request.session = {}
+        request.user = reviewer1
+
+        data = {
+            "review": review.pk,
+            "reviewer": Reviewer.objects.get(user=reviewer1, review=review).pk,
+            "review_status": ModelReview.APPROVED,
+        }
+
+        form = PerformReview(data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        review.refresh_from_db()
+        self.assertEqual(ModelReview.PENDING, review.review_status)
+
+        # test that reviewer2 got email to go and review
+        self.assertEqual(2, mock.call_count)
+        mock.assert_called_with(
+            reviewer=Reviewer.objects.get(user=reviewer2, review=review, level=1,)
+        )
+
+        # let reviewer2 approve it
+        request = self.factory.get("/")
+        request.session = {}
+        request.user = reviewer1
+
+        data = {
+            "review": review.pk,
+            "reviewer": Reviewer.objects.get(user=reviewer2, review=review).pk,
+            "review_status": ModelReview.APPROVED,
+        }
+
+        form = PerformReview(data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        review.refresh_from_db()
+        self.assertEqual(ModelReview.PENDING, review.review_status)
+
+        # test that reviewer3 got email to go and review
+        self.assertEqual(3, mock.call_count)
+        mock.assert_called_with(
+            reviewer=Reviewer.objects.get(user=reviewer3, review=review, level=2,)
+        )
+
+        # let reviewer3 approve it
+        request = self.factory.get("/")
+        request.session = {}
+        request.user = reviewer1
+
+        data = {
+            "review": review.pk,
+            "reviewer": Reviewer.objects.get(user=reviewer3, review=review).pk,
+            "review_status": ModelReview.APPROVED,
+        }
+
+        form = PerformReview(data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        # test that another request to review email was not sent
+        self.assertEqual(3, mock.call_count)
+
+        review.refresh_from_db()
+        self.assertEqual(ModelReview.APPROVED, review.review_status)
