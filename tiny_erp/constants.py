9c867af5aaba62af47157c5ab4be6522df2349e6
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-15 21:18:33 +0300

    Add constants for requisition filed emails

--- tiny_erp/constants.py
+++ tiny_erp/constants.py
@@ -2,3 +2,7 @@
 EMAIL_TEMPLATE_PATH = "tiny_erp/email"
 REQUISITION_FILED_EMAIL_TEMPLATE = "requisition_filed"
 REQUISITION_COMPLETED_EMAIL_TEMPLATE = "requisition_completed"
+REQUISITION_FILED_EMAIL_SUBJ = "New Purchase Requisition"
+REQUISITION_FILED_EMAIL_TXT = (
+    "There has been a new purchase requisition.  Please log in to process it."
+)
