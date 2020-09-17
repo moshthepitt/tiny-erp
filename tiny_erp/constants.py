764beddcd431a6c47363d9b301feb8b7303f533b
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-17 22:11:05 +0300

    Add more constants

--- tiny_erp/constants.py
+++ tiny_erp/constants.py
@@ -6,3 +6,7 @@ REQUISITION_FILED_EMAIL_SUBJ = "New Purchase Requisition"
 REQUISITION_FILED_EMAIL_TXT = (
     "There has been a new purchase requisition.  Please log in to process it."
 )
+REQUISITION_COMPLETED_EMAIL_SUBJ = "Purchase Requisition Processed"
+REQUISITION_COMPLETED_EMAIL_TXT = (
+    "The purchase requisition has been processed.  Please log in to view it."
+)
