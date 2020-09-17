d304fc4781032993e7af3e5da56b2dc2516826a7
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-17 22:13:59 +0300

    Add more snapshots

--- tests/snapshots/snap_test_emails.py
+++ tests/snapshots/snap_test_emails.py
@@ -16,13 +16,35 @@ Department: Science
 Location: Voi
 Staff Member: Bob Ndoe
 
-Please log in to process the above: http://example.com/reviews/1
+Please log in to process the above: http://example.com/reviews/2
 
 Thank you,
 
 
 example.com
 ------
-http://example.com'''
+http://example.com
+'''
 
-snapshots['TestEmails::test_requisition_filed_email 2'] = 'Bob Ndoe sent this purchase requisition:<br /><br />Date Placed: Jan. 1, 2019<br />Date Required: Feb. 2, 2019<br />Business: X Inc<br />Department: Science<br />Location: Voi<br />Staff Member: Bob Ndoe<br />Please log in to process the above: http://example.com/reviews/1<br /><br />Thank you,<br />example.com<br />------<br />http://example.com'
+snapshots['TestEmails::test_requisition_filed_email 2'] = 'Bob Ndoe sent this purchase requisition:<br /><br />Date Placed: Jan. 1, 2019<br />Date Required: Feb. 2, 2019<br />Business: X Inc<br />Department: Science<br />Location: Voi<br />Staff Member: Bob Ndoe<br />Please log in to process the above: http://example.com/reviews/2<br /><br />Thank you,<br />example.com<br />------<br />http://example.com'
+
+snapshots['TestEmails::test_requisition_approved_email 1'] = '''Your purchase requisition has been approved.
+
+Date Placed: April 3, 2019
+Date Required: April 10, 2019
+Business: X Inc
+Department: Science
+Location: Voi
+Status: Approved
+
+You can log in to view it at: http://example.com/reviews/1
+
+Thank you,
+
+
+example.com
+------
+http://example.com
+'''
+
+snapshots['TestEmails::test_requisition_approved_email 2'] = 'Your purchase requisition has been approved.<br /><br />Date Placed: April 3, 2019<br />Date Required: April 10, 2019<br />Business: X Inc<br />Department: Science<br />Location: Voi<br />Status: Approved<br />You can log in to view it at: http://example.com/reviews/1<br /><br />Thank you,<br />example.com<br />------<br />http://example.com'
