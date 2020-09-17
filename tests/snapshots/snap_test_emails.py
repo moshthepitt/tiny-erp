2bf79316f5e5ba1d7c5c20d1f822974e0f2fb392
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-17 22:58:22 +0300

    Update snapshot

--- tests/snapshots/snap_test_emails.py
+++ tests/snapshots/snap_test_emails.py
@@ -28,7 +28,7 @@ http://example.com
 
 snapshots['TestEmails::test_requisition_filed_email 2'] = 'Bob Ndoe sent this purchase requisition:<br /><br />Date Placed: Jan. 1, 2019<br />Date Required: Feb. 2, 2019<br />Business: X Inc<br />Department: Science<br />Location: Voi<br />Staff Member: Bob Ndoe<br />Please log in to process the above: http://example.com/reviews/13<br /><br />Thank you,<br />example.com<br />------<br />http://example.com'
 
-snapshots['TestEmails::test_requisition_approved_email 1'] = '''Your purchase requisition has been approved.
+snapshots['TestEmails::test_requisition_completed_email 1'] = '''Your purchase requisition has been approved.
 
 Date Placed: April 3, 2019
 Date Required: April 10, 2019
@@ -47,4 +47,4 @@ example.com
 http://example.com
 '''
 
-snapshots['TestEmails::test_requisition_approved_email 2'] = 'Your purchase requisition has been approved.<br /><br />Date Placed: April 3, 2019<br />Date Required: April 10, 2019<br />Business: X Inc<br />Department: Science<br />Location: Voi<br />Status: Approved<br />You can log in to view it at: http://example.com/reviews/12<br /><br />Thank you,<br />example.com<br />------<br />http://example.com'
+snapshots['TestEmails::test_requisition_completed_email 2'] = 'Your purchase requisition has been approved.<br /><br />Date Placed: April 3, 2019<br />Date Required: April 10, 2019<br />Business: X Inc<br />Department: Science<br />Location: Voi<br />Status: Approved<br />You can log in to view it at: http://example.com/reviews/12<br /><br />Thank you,<br />example.com<br />------<br />http://example.com'
