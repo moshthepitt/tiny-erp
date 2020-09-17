f1d1738c6a455d8b6cdd06ce1ace90b6ef51a21e
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-17 22:38:05 +0300

    Remove unused settings

--- tiny_erp/settings.py
+++ tiny_erp/settings.py
@@ -21,9 +21,3 @@ TINY_ERP_REQUISITION_REVIEWERS: List[str] = []
 TINY_ERP_REQUISITION_REVIEWS_TIERS: bool = False
 TINY_ERP_ADMIN_EMAILS = [settings.DEFAULT_FROM_EMAIL]  # should remove?
 TINY_ERP_ACCOUNTS_EMAILS = [settings.DEFAULT_FROM_EMAIL]  # should remove?
-# TINY_ERP_REQUISITION_FILED_EMAIL_TXT - body of requisition filed email
-# TINY_ERP_REQUISITION_FILED_EMAIL_SUBJ - subject of requisition filed email
-# TINY_ERP_REQUISITION_UPDATED_EMAIL_TXT - body of requisition updated email
-# TINY_ERP_REQUISITION_UPDATED_EMAIL_SUBJ - - subject of requisition updated email
-# TINY_ERP_REQUISITION_APPROVED_EMAIL_TXT - body of requisition updated email
-# TINY_ERP_REQUISITION_APPROVED_EMAIL_SUBJ - - subject of requisition updated email
