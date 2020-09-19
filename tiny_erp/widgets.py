e2291f05fc34175fa8d38588c8e45b3d3339010b
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-19 11:30:04 +0300

    Fix docstrings

--- tiny_erp/widgets.py
+++ tiny_erp/widgets.py
@@ -1,13 +1,12 @@
-"""widgets module"""
+"""widgets module."""
 from django.forms.widgets import Textarea
 
 
 class MiniTextarea(Textarea):
-    """
-    Vertically shorter version of textarea widget.
-    """
+    """Vertically shorter version of textarea widget."""
 
     rows = 2
 
     def __init__(self, attrs=None):
+        """Initialize."""
         super().__init__({"rows": self.rows})
