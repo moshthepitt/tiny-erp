9f14a70e9eeff83470f4c3c3b5210b941b51d45b
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-10-16 22:29:29 +0300

    ↑ bump to version 0.3.0

--- tiny_erp/__init__.py
+++ tiny_erp/__init__.py
@@ -1,6 +1,6 @@
 """Main init file for tiny_erp."""
-VERSION = (0, 2, 2)
+VERSION = (0, 3, 0)
 
 __vega_admin_version__ = "0.0.19"
-__small_small_hr_version__ = "0.2.2"
+__small_small_hr_version__ = "0.4.0"
 __version__ = ".".join(str(_) for _ in VERSION)
