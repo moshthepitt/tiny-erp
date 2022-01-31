f03de9791026259978833bbdff6892e6e7cf8d8d
Kelvin Jayanoris <kjayanoris@gmail.com>
2022-01-31 17:14:10 +0300

    Upgrade small-small-hr

--- tiny_erp/__init__.py
+++ tiny_erp/__init__.py
@@ -1,6 +1,6 @@
 """Main init file for tiny_erp."""
-VERSION = (0, 3, 0)
+VERSION = (0, 4, 0)
 
 __vega_admin_version__ = "0.0.19"
-__small_small_hr_version__ = "0.4.0"
+__small_small_hr_version__ = "0.4.1"
 __version__ = ".".join(str(_) for _ in VERSION)
